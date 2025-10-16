5be68145ef2f0ea9180e79408fb1adae33573484
SimplyEben <afriyientiamoah784@gmail.com>
2025-10-16 16:17:41 +0000

    Cleaning up the genres

--- src/my-components/GenreList.tsx
+++ src/my-components/GenreList.tsx
@@ -1,6 +1,13 @@
 import useGenres, { type Genre } from "@/hooks/useGenres";
 import getCroppedImageUrl from "@/services/image-url";
-import { HStack, List, Image, Spinner, Button } from "@chakra-ui/react";
+import {
+  HStack,
+  List,
+  Image,
+  Spinner,
+  Button,
+  Heading,
+} from "@chakra-ui/react";
 
 interface Props {
   //when a genre is selected, the GenreList will notify the App component through this prop (onSelectGenre) which is a function that takes a genre object.
@@ -13,29 +20,37 @@ function GenreList({ onSelectGenre, selectedGenre }: Props) {
   if (error) return null;
   if (isLoading) return <Spinner />;
   return (
-    <List.Root>
-      {data.map((genre) => (
-        <List.Item key={genre.id} paddingY="5px" listStyle="none">
-          <HStack>
-            <Image
-              boxSize="32px"
-              borderRadius={8}
-              src={getCroppedImageUrl(genre.image_background)}
-            />
-            {/* passing the selected genre to onSelectGenre to update the state in the App component */}
-            <Button
-              variant="ghost"
-              fontSize="md"
-              asChild
-              onClick={() => onSelectGenre(genre)}
-              fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
-            >
-              <a href="#">{genre.name}</a>
-            </Button>
-          </HStack>
-        </List.Item>
-      ))}
-    </List.Root>
+    <>
+      <Heading fontSize="2xl" marginBottom={2}>
+        Genres
+      </Heading>
+      <List.Root>
+        {data.map((genre) => (
+          <List.Item key={genre.id} paddingY="5px" listStyle="none">
+            <HStack>
+              <Image
+                boxSize="32px"
+                borderRadius={8}
+                objectFit="cover"
+                src={getCroppedImageUrl(genre.image_background)}
+              />
+              {/* passing the selected genre to onSelectGenre to update the state in the App component */}
+              <Button
+                variant="ghost"
+                fontSize="lg"
+                as="a"
+                whiteSpace="normal"
+                onClick={() => onSelectGenre(genre)}
+                fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
+                paddingLeft="3px"
+              >
+                {genre.name}
+              </Button>
+            </HStack>
+          </List.Item>
+        ))}
+      </List.Root>
+    </>
   );
 }
 
